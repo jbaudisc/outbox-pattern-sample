81e82014800a4a57c51ea6c3dd86410b7782a003
Dustin Lapierre <dustin.lapierre@bdess.com>
2023-01-10 16:30:51 +0100

    Added example consumer

---
+++ src/main/java/com/github/hpgrahsl/ms/outbox/sample/consumer/ExampleConsumer.java
@@ -0,0 +1,17 @@
+package com.github.hpgrahsl.ms.outbox.sample.consumer;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleConsumer {
+    @KafkaListener(topics = "schema-changes.outbox-demo")
+    public void read(ConsumerRecord<String, GenericRecord> record){
+        //get data from record
+        GenericRecord order=record.value();
+        //print it
+        System.out.println(order.toString());
+    }
+}
